bb07fd6d319083361a3836ef6cb7996f74590e4d
edeguzman05 <edeguzmanjr05@csu.fullerton.edu>
2026-06-04 14:08:17 -0700

    Updated style.css and js
    I wanted to make it much more usable on mobile devices , and I added more to the about me " section o that more can learn from me.

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ const pageData = {
                     <h3> Duelist: Gara </h3>
                     <p> Otherwise known as Emmanuel De Guzman, I've been in computer science since high school starting within my tech class with HTML/CSS and JS being my first language </p>
                     <p> I am currently pursuing a degree in Computer Science at California State University, Fullerton, and have plan on getting a master's degree in the future. Though I plan to work in the field as well, possibly pursuing a career in software development </p>
+                    <p> A lot of my ideas stem from my experiences and the problems I encounter in my daily life. I also really want to change the world and have a much more profound and positive impact using the technology that I have been graced with. This also means helping those with special needs and developing new and innovative tools to help them feel included. </p>
                     <img class="hologram-image" src="personalSRC/selfie.jpg">
                     <p> I enjoy problem solving and logical thinking, with a passion for creating an elegant yet effective solution to problems in today's world. </p>
                     <p> I have interest in machine learning, databases management, algorithms, data structure implementation, and software development.</p>
@@ -36,7 +37,7 @@ const pageData = {
                         <p> Skills: Problem Solving, Logical Thinking, Teamwork, Communication, Patience, Adaptability </p>
 
                         <p> I have experience using various tools not related to programming, such as Adobe Photoshop, Illustrator, Premiere Pro and some minimal experience with After Effects.</p>
-                        <p> Outside of CS, I work as a Special Education Instructional Aide at Arcadia Unified School District, where I assist students with special needs in their academic and social development. This role has enhanced my communication skills, patience, and ability to adapt to different learning styles. </p>
+                        <p> Outside of CS, I work as a Special Education Instructional Aide at Arcadia Unified School District, where I assist students with special needs in their academic and social development. This role has enhanced my communication skills, patience, and ability to adapt to different learning styles. This includes students with autism, ADHD, and other developmental disabilities. </p>
 
                         <p> I have also worked as a tutor at my high school as well, where I provided academic support to students in various subjects, including math for summer school. </p>
                         <p> I have also worked on different projects, often trying to do solo projects, such as  discord bot, building a personal website, and creating simple games.</p>
@@ -55,7 +56,7 @@ const pageData = {
                         <img class="hologram-image" src="personalSRC/tennis.jpg">
                         <p> I have a passion for music as well, listening to a variety of genres such as rap, hip-hop, kpop, metal, rock and much more. I love listening to music while working on playing video games or even building a deck in Yu-Gi-Oh </p>
                         <p> I really enjoy Round 1, the arcade, and I love trying out new sports as well and working out.</p>
-                        <p> I really love editing and producing videos.
+                        <p> I really love editing and producing videos as a past time as well, especially making gaming skits for a friend and his channel.</p>
             </div>
         `
     },
@@ -105,7 +106,10 @@ const pageData = {
     'projects-card': { loading: '<h2>System</h2><p>Initializing...</p>', actual: '<p>Projects coming soon...</p>' },
     
     'contact-card': { loading: `<h2>System</h2><p>Initializing...</p>`, 
-                      actual: `<p> Email: edeguzmanjr05@gmail.com </p>
+                      actual: `<div class="hologram-scroll-area">
+                                    <div class="scroll-card">
+                                        <h3> Contact Information </h3>
+                                        <p> Email: edeguzmanjr05@gmail.com </p>
         `
     }
 };
